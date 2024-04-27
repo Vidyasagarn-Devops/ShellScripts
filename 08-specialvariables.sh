663d874a41e403b9aac14f4212d023afaa749b27
Vidyasagar <vidyasagar.nellipudi@gmail.com>
2024-04-28 00:45:46 +0530

    sending

--- 08-specialvariables.sh
+++ 08-specialvariables.sh
@@ -3,3 +3,8 @@
 echo "all variables : $@"
 echo " Toatl Variables passed : $#"
 echo " Script name : $0"
+echo "current working directory : $PWD"
+echo "home directory of current user : $HOME"
+echo "which user is running : $USER"
+echo " hostname : $HOSTNAME"
+echo "process id of current shell script : $$"
