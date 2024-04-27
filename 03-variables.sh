b59296332d7336b8b486b978fce6b8977d5551de
Vidyasagar <vidyasagar.nellipudi@gmail.com>
2024-04-27 23:41:00 +0530

    sending

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+P1=Ramesh
+P2=Suresh
+
+echo "$P1 : Hello $P2, How are you?"
+echo "$P2 : Hi $P1, I am Fine. How are you?"
+echo "$P1 : I am fine too. how is your work?"
+echo "$P2 : not bad,, I am thinking to upgrade to Devops"
